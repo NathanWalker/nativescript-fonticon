ef39ab411e0d917032e67cad60b3128c0bee64d8
Christopher Fenn <c.fenn@fis-gmbh.de>
2018-06-28 09:26:44 +0200

    use mocha to be able to run multiple tests at once

--- test/mapcss.spec.ts
+++ test/mapcss.spec.ts
@@ -1,51 +1,65 @@
 import * as fs from 'fs'
 import * as assert from 'assert'
+import { describe, it, before } from 'mocha'
 import * as lib from '../lib'
 
-let fileContent = ''
+describe('Test mapCss function', () => {
 
-try {
-  fileContent = fs.readFileSync('test/font-awesome.css', 'utf8')
-} catch (err) {
-  throw err
-}
+  let fileContent = ''
+  let map
 
-const map = lib.mapCss(fileContent, true)
+  const expected = {
+    'fa-glass': '\uf000',
+    'fa-music': '\uf001',
+    'fa-search': '\uf002',
+    'fa-envelope-o': '\uf003',
+    'fa-heart': '\uf004',
+    'fa-remove': '\uf00d',
+    'fa-close': '\uf00d',
+    'fa-times': '\uf00d',
+    'fa-gear': '\uf013',
+    'fa-cog': '\uf013',
+    'fa-rotate-right': '\uf01e',
+    'fa-repeat': '\uf01e',
+    'fa-dedent': '\uf03b',
+    'fa-outdent': '\uf03b',
+    'fa-photo': '\uf03e',
+    'fa-image': '\uf03e',
+    'fa-picture-o': '\uf03e',
+  }
 
-const expected = {
-  'fa-glass': '\uf000',
-  'fa-music': '\uf001',
-  'fa-search': '\uf002',
-  'fa-envelope-o': '\uf003',
-  'fa-heart': '\uf004',
-  'fa-remove': '\uf00d',
-  'fa-close': '\uf00d',
-  'fa-times': '\uf00d',
-  'fa-gear': '\uf013',
-  'fa-cog': '\uf013',
-  'fa-rotate-right': '\uf01e',
-  'fa-repeat': '\uf01e',
-  'fa-dedent': '\uf03b',
-  'fa-outdent': '\uf03b',
-  'fa-photo': '\uf03e',
-  'fa-image': '\uf03e',
-  'fa-picture-o': '\uf03e',
-}
+  before('load css file to test', () => {
+    try {
+      fileContent = fs.readFileSync('test/font-awesome.css', 'utf8')
+    } catch (err) {
+      throw err
+    }
+  })
 
-assert.equal(Object.keys(map).length, Object.keys(expected).length)
+  it('parses without errors', () => {
+    map = lib.mapCss(fileContent)
+  })
 
-const assertPropExistsWithValue = (map, key, value) => {
-  assert.ok(map.hasOwnProperty(key))
-  assert.ok(typeof map[key] === 'string')
-  assert.strictEqual(
-    map[key],
-    value,
-    `Values for key "${key}" do not match.` +
-    ` Expected: "${value}" (charcode ${value.charCodeAt(0)}),` +
-    ` actual: "${map[key]}" (charcode ${map[key].charCodeAt(0)})`
-  )
-}
+  it('has expected number of keys', () => {
+    assert.equal(Object.keys(map).length, Object.keys(expected).length)
+  })
 
-Object.keys(expected).forEach((key) => {
-  assertPropExistsWithValue(map, key, expected[key])
+  it('has expected keys and values', () => {
+    const assertPropExistsWithValue = (map, key, value) => {
+      assert.ok(map.hasOwnProperty(key))
+      assert.ok(typeof map[key] === 'string')
+      assert.strictEqual(
+        map[key],
+        value,
+        `Values for key "${key}" do not match.` +
+        ` Expected: "${value}" (charcode ${value.charCodeAt(0)}),` +
+        ` actual: "${map[key]}" (charcode ${map[key].charCodeAt(0)})`
+      )
+    }
+
+    Object.keys(expected).forEach((key) => {
+      assertPropExistsWithValue(map, key, expected[key])
+    })
+  })
+
 })
