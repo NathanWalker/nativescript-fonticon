b39a998f0424f4dd5cde8590f9c131eb5277381b
Christopher Fenn <c.fenn@fis-gmbh.de>
2018-06-28 10:10:10 +0200

    moved cleanValue to a separate function and added tests

--- lib.ts
+++ lib.ts
@@ -1,10 +1,6 @@
 export const mapCss = (data: any, debug?: boolean): object => {
   const map = {}
   let sets = data.split('}');
-  let cleanValue = (val) => {
-    let v = val.replace(/content:\s*"\\f([^"]+)"/, '\\uf$1').trim();
-    return v;
-  };
 
   for (let set of sets) {
     let pair = set.split(/:before\s*{/);
@@ -12,6 +8,9 @@ export const mapCss = (data: any, debug?: boolean): object => {
     let keys = keyGroups.split(',');
     if (pair[1]) {
       let value = cleanValue(pair[1]);
+      if (!value) {
+        continue;
+      }
       for (let key of keys) {
         key = key.trim().slice(1).split(':before')[0];
         map[key] = String.fromCharCode(parseInt(value.substring(2), 16));
@@ -22,4 +21,12 @@ export const mapCss = (data: any, debug?: boolean): object => {
     }
   }
   return map
+}
+
+export const cleanValue = (val: string): string | void => {
+  const matches = val.match(/content:\s*"\\f([^"]+)"/i)
+  if (matches) {
+    return `\\uf${matches[1]}`
+  }
+  return void 0;
 }
